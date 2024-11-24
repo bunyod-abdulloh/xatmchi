0db4d1d22a1543e3a365353d3176da53980e8efc
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-11-24 20:38:27 +0500

    Commited

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import json
+import middlewares, filters, handlers
+
 from aiogram import executor
 
-from loader import dp, db
-import middlewares, filters, handlers
+from loader import dp, db, bot
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
+
 
 async def on_startup(dispatcher):
     await db.create()
@@ -19,12 +21,11 @@ async def on_startup(dispatcher):
     list1 = [n for n in range(1, 31)]
     await db.add_xatm_db(id=1, list=json.dumps(list1), xatm_data="{}")
     await db.add_users_stat(0, "Poralar taqsimlash holati", 1, 1)
-
+    await bot.delete_my_commands()
     await set_default_commands(dispatcher)
 
     await on_startup_notify(dispatcher)
 
+
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
-
-
