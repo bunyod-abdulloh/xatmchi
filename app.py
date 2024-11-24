917efaa3be67699f4aaf78eb3967eda63e1d21de
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-11-24 21:09:04 +0500

    Commited

--- app.py
+++ app.py
@@ -9,22 +9,25 @@ from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    await db.create()
-    # await db.drop_xatmusers()
-    # await db.drop_table_xatm_db()
-    # await db.drop_table_users_stat()
+    try:
+        await db.create()
+        # await db.drop_xatmusers()
+        # await db.drop_table_xatm_db()
+        # await db.drop_table_users_stat()
 
-    await db.create_table_stat()
-    await db.create_table_xatmusers()
-    await db.create_table_xatm_db()
+        await db.create_table_stat()
+        await db.create_table_xatmusers()
+        await db.create_table_xatm_db()
 
-    list1 = [n for n in range(1, 31)]
-    await db.add_xatm_db(id=1, list=json.dumps(list1), xatm_data="{}")
-    await db.add_users_stat(0, "Poralar taqsimlash holati", 1, 1)
-    await bot.delete_my_commands()
-    await set_default_commands(dispatcher)
+        list1 = [n for n in range(1, 31)]
+        await db.add_xatm_db(id=1, list=json.dumps(list1), xatm_data="{}")
+        await db.add_users_stat(0, "Poralar taqsimlash holati", 1, 1)
+        await bot.delete_my_commands()
+        await set_default_commands(dispatcher)
 
-    await on_startup_notify(dispatcher)
+        await on_startup_notify(dispatcher)
+    except:
+        pass
 
 
 if __name__ == '__main__':
